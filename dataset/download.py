171c596188215758ed051967080df1b6e5324dca
choutianxius <choutianxius@gmail.com>
2024-11-19 00:20:32 -0600

    Download 50 random categories of simplified Quick, Draw! .ndjson data

---
+++ dataset/download.py
@@ -0,0 +1,41 @@
+"""
+Download 50 randomly selected categories of the Quick, Draw! dataset as .ndjson files.
+"""
+
+import random
+import os
+import pathlib
+import requests
+from tqdm import tqdm
+
+
+random.seed(123456)
+
+# read original categories
+# see https://github.com/googlecreativelab/quickdraw-dataset/blob/master/categories.txt
+categories_path = os.path.join(pathlib.Path(__file__).parent, "categories.txt")
+
+with open(categories_path, "r") as f:
+    categories = list(filter(bool, [line.strip() for line in f.readlines()]))
+
+# select 50 categories
+categories_50 = random.choices(categories, weights=None, k=50)
+
+categories_50_path = os.path.join(pathlib.Path(__file__).parent, "categories_50.txt")
+with open(categories_50_path, "w") as f:
+    f.write("\n".join(categories_50))
+
+# download
+data_dir_path = os.path.join(
+    pathlib.Path(__file__).parent.parent, "data", "original_simplified"
+)
+if not os.path.exists(data_dir_path):
+    os.makedirs(data_dir_path)
+for category in tqdm(categories_50):
+    url = f"https://storage.googleapis.com/quickdraw_dataset/full/simplified/{category}.ndjson"
+    save_path = os.path.join(data_dir_path, f"{category}.ndjson")
+    res = requests.get(url, stream=True)
+    with open(save_path, "wb") as f:
+        for chunk in res.iter_content(chunk_size=8192):
+            if chunk:
+                f.write(chunk)
